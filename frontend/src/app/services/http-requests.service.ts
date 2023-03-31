108ade29c814f49a8c4816694d92972183d7329d
Bucki <65296674+Benjibucki@users.noreply.github.com>
2023-03-31 13:13:12 +0200

    changement baseUri

--- frontend/src/app/services/http-requests.service.ts
+++ frontend/src/app/services/http-requests.service.ts
@@ -4,31 +4,38 @@ import { Observable } from 'rxjs';
 import { Api_httpInterceptor } from '../Class/Api_httpInterceptor';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HttpRequestsService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor(private httpClient : HttpClient) { }
-
-  public login(login:string,password:string): Observable<any>{
+  public login(login: string, password: string): Observable<any> {
     const formData = new FormData();
     formData.append('login', login);
     formData.append('pass', password);
-    return this.httpClient.post<any> ("http://localhost:8081/api/login",formData);
+    return this.httpClient.post<any>(
+      'https://cnam-tp05-gallier-benjamin.onrender.com/api/login',
+      formData
+    );
   }
 
-  public GetUsers(): Observable<any>{
+  public GetUsers(): Observable<any> {
     //Api_httpInterceptor.jwtToken="eyJhbGciOiJIUzI1NiJ9.e30.ECFeffV1yM9iD8t7d3YA_xytwbz8QS-DoG0-KtKnYQo";
-    return this.httpClient.get<any> ("http://localhost:8081/api/user");
-  } 
+    return this.httpClient.get<any>(
+      'https://cnam-tp05-gallier-benjamin.onrender.com/api/user'
+    );
+  }
 
-  public GetCatalogue(): Observable<any>{
+  public GetCatalogue(): Observable<any> {
     //Api_httpInterceptor.jwtToken="eyJhbGciOiJIUzI1NiJ9.e30.ECFeffV1yM9iD8t7d3YA_xytwbz8QS-DoG0-KtKnYQo";
-    return this.httpClient.get<any> (`http://localhost:8081/api/catalogue`);
-  } 
+    return this.httpClient.get<any>(
+      `https://cnam-tp05-gallier-benjamin.onrender.com/api/catalogue`
+    );
+  }
 
-  public Hello(name:string): Observable<any>{
-    return this.httpClient.get<any> (`http://localhost:8081/api/hello/${name}`);
-  } 
-
+  public Hello(name: string): Observable<any> {
+    return this.httpClient.get<any>(
+      `https://cnam-tp05-gallier-benjamin.onrender.com/api/hello/${name}`
+    );
+  }
 }
